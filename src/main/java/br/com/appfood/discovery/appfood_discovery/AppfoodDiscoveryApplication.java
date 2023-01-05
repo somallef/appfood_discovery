d042b8bed590d6df5dc997d19a6d7045277a2143
Allef Mizael <allef.mizael@gmail.com>
2023-01-04 22:01:10 -0300

    primeiro commit

---
+++ src/main/java/br/com/appfood/discovery/appfood_discovery/AppfoodDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.appfood.discovery.appfood_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfoodDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfoodDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/appfood/discovery/appfood_discovery/AppfoodDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appfood.discovery.appfood_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfoodDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
